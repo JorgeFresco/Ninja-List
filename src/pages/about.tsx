46e18913310828908924fab7fc85581e21b3b12c
JorgeFresco <jn.fresco@campus.fct.unl.pt>
2023-03-24 10:24:47 +0000

    Initial files

---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum aspernatur unde quos dolore! Voluptatem fugit quod numquam ut tempore omnis ratione laudantium error aliquam. Officia expedita fugit molestiae nemo commodi?</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat at officia sed qui deleniti, beatae non ad, illum laborum, aperiam commodi libero quam! Odio possimus dolore qui similique itaque sunt.</p>
+        </div>
+    );
+}
+
+export default About;
